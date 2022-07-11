fc7d84b1761475a1394fa2324a16c9853685a970
AlvaroAndreyCVera <107379588+AlvaroAndreyCVera@users.noreply.github.com>
2022-07-11 13:49:25 -0500

    a
    a

---
+++ main.py
@@ -0,0 +1,10 @@
+def  devolver_perimetro ( a ):
+    un  = ( un  *  4 )
+    devolver  un
+
+
+# bloque principal del programa
+lado  =  int ( input ( "Ingrese el valor del lado del cuadrado: " ))
+imprimir ( "----------------------------------------------------" )
+perimetro  =  retornar_perimetro ( lado )
+print ( "El perimetro del cuadrado es: " , perimetro )
